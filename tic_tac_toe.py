975c400db90632ce44a6e1cdb5a10cee721c10ad
Warren Bain <warren@thoughtcroft.com>
2017-05-30 22:10:17 +1000

    Add clear screen between moves

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#pylint: disable=redefined-builtin
+# pylint: disable=redefined-builtin
 
 """ Tic Tac Toe game
 
@@ -12,8 +12,6 @@ a space represents an unoccupied position
 # python2 and python3 portability
 from __future__ import print_function
 from builtins import input
-import sys
-
 
 def display_board(board):
     """Print the current board layout"""
@@ -25,12 +23,10 @@ def display_board(board):
     print(" {} | {} | {} ".format(*board[6:9]))
     print()
 
-
 def make_move(board, position, player):
     """Add player's move to the board"""
     # only valid moves are passed in here
     board[position-1] = player
-
 
 def winning_move(board, position, player):
     """Check if the player made a winning move"""
@@ -44,18 +40,15 @@ def winning_move(board, position, player):
         return get_diagonal(board, 1) == win or get_diagonal(board, 3) == win
     return False
 
-
 def get_row(board, position):
     """Return the row result"""
     row = (position-1) // 3 * 3
     return board[row:row+3]
 
-
 def get_column(board, position):
     """Return the column result"""
     col = (position-1) % 3
     return [board[x] for x in (col, col+3, col+6)]
-
 
 def get_diagonal(board, diagonal):
     """Return the diagonal result based on 1 (back) or 3 (forward)"""
@@ -64,7 +57,6 @@ def get_diagonal(board, diagonal):
     elif diagonal == 3:
         positions = (2, 4, 6)
     return [board[x] for x in positions]
-
 
 def get_next_move(board, player):
     """Prompt player to enter their next move"""
@@ -78,10 +70,13 @@ def get_next_move(board, player):
         print("That is not a valid move, please try again...")
     return move
 
-
 def valid_moves(board):
     """Return available moves ie unoccupied index positions"""
     return [i for i, x in enumerate(board) if x == ' ']
+
+def clear_screen():
+    """Clear the screen"""
+    print("\033[H\033[J")
 
 
 def main():
@@ -97,34 +92,43 @@ def main():
     To make a move, please enter a position from 1 - 9
     corresponding to the layout of your phone keypad
     """)
+    input("Hit enter to continue...")
 
-    while True:
-        board = list(' ' * 9)
-        players = ['X', 'O']
-        moves = 0
-        display_board(board)
-
+    try:
         while True:
-            player = players.pop(0)
-            players.append(player)
-            moves += 1
-            position = get_next_move(board, player)
-            make_move(board, position, player)
+            clear_screen()
+            board = list(' ' * 9)
+            players = ['X', 'O']
+            moves = 0
             display_board(board)
 
-            if moves > 4 and winning_move(board, position, player):
-                print("Congratulations '{}', you are the winner :)".format(player))
-                break
+            while True:
+                player = players.pop(0)
+                players.append(player)
+                moves += 1
+                position = get_next_move(board, player)
+                make_move(board, position, player)
+                clear_screen()
+                display_board(board)
 
-            if moves > 8:
-                print("Stalemate, no-one is a winner :(")
-                break
+                if moves > 4 and winning_move(board, position, player):
+                    print("Congratulations '{}', you are the winner :)".format(player))
+                    break
 
+                if moves > 8:
+                    print("Stalemate, no-one is a winner :(")
+                    break
+
+            print()
+            reply = input("Would you like to play again (Y/n)?").upper()
+            if reply != 'Y' and reply != '':
+                break
+    except KeyboardInterrupt:
         print()
-        print("Would you like to play again (y/n)?")
-        if input('> ').upper() != 'Y':
-            break
+    finally:
+        print()
+        print("Thanks for playing!")
+        print()
 
-    sys.exit(0)
-
-main()
+if __name__ == '__main__':
+    main()
